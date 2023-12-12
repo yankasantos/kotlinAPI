f2060ab2bee26c3d41d17b352fa0a978398e2eb8
Yanka Santos <yanka.santos@thoughtworks.com>
2023-12-12 19:01:42 -0300

    Refactoring the method to get keyboard value to search on API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,18 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.Scanner
 
 
 fun main() {
+    val readKeyboard = Scanner(System.`in`)
+    println("Insert a code of game")
+    val search = readKeyboard.nextLine()
+
+    val linkAdress = "https://www.cheapshark.com/api/1.0/games?id=$search"
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=612"))
+        .uri(URI.create(linkAdress))
         .build()
     val response = client
         .send(request, BodyHandlers.ofString())
